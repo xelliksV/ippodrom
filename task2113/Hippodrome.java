225af8427156ada1e0c33b75b46aa17123de7f7d
xelliksV <xelliks666@gmail.com>
2024-12-06 05:19:48 +0700

    Added list for horses

--- task2113/Hippodrome.java
+++ task2113/Hippodrome.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task21.task2113;
 
 public class Hippodrome {
+    private List<Horse> horses = new ArrayList<>();
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
     public static void main(String[] args) {
 
     }
